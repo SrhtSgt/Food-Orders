15e1b19ba6fb460b22e1d04cb0bf64c566d1ac20
Serhat Söğüt <serhatsoguts@gmail.com>
2023-03-07 19:45:34 +0300

    Menülist Maplemesi Yapıldı

--- client/src/components/MenuList.jsx
+++ client/src/components/MenuList.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function MenuList() {
-  return <div>MenuList</div>;
+function MenuList({ menu }) {
+  return (
+    <div>
+      <div className="card m-auto my-3" style={{ width: "18rem" }}>
+        <img src={menu.img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{menu.ad}</h5>
+          <p className="card-text">{menu.desc}</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MenuList;
